a2d28983327dfe13aa31c29e475b35dcd1a8ff6c
Oksana Lyalka <lyalka25@gmail.com>
2018-10-07 13:14:51 +0300

    /Users/oksanalyalka/Lyalka_python2018/week1_1

---
+++ Lyalka_python2018/week1_1/math.py
@@ -0,0 +1,9 @@
+
+a = input("a = ")
+b = input("b = ")
+c = int(a) + int(b)
+d = int(a) - int(b)
+
+
+print ("a + b =", c)
+print ("a - b =", d)
---
+++ Lyalka_python2018/week1_1/name_age.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+place_of_living = input("Where do you live? ")
+
+print ("Hello,", name)
+print ("Your age", age)
+print ("You live in", place_of_living)

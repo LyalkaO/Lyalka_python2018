0b424054e7b83b09365086fceadcd9ede2bee065
Oksana Lyalka <lyalka25@gmail.com>
2018-10-21 16:31:46 +0300

    week1_2

--- Lyalka_python2018/week1_2/jenny.py
+++ Lyalka_python2018/week1_2/jenny.py
@@ -1,6 +1,4 @@
 def greet(name):
    if name == "Johnny":
-       print ("Hello, my love!")
-   else:
-       print ("Hello, {name}!".format(name=name))
-
+       return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)

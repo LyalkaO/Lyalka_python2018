76710fd933dfe41a876ee360dce7eec010a1e0ff
Oksana Lyalka <lyalka25@gmail.com>
2018-10-21 20:20:29 +0300

    week_2_1

--- Lyalka_python2018/week_2_1/bus.py
+++ Lyalka_python2018/week_2_1/bus.py
@@ -1,5 +1,10 @@
+# def enough(cap, on, wait):
+#     if cap-on >= wait:
+#         return 0
+#     else: cap-on-1 < wait
+#     return wait-cap+on
+
 def enough(cap, on, wait):
-    if cap-on >= wait:
-        return 0
-    else: cap-on-1 < wait
-    return wait-cap+on
+    return 0 if cap-on >= wait else wait-cap+on
+
+
